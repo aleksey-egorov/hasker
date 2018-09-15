e6759d2a6c5a83e3f12a3922c1ff8b719d98b0f6
aleksey <a.egorov@daiteco.ru>
2018-09-15 18:25:22 +0300

    update

--- hasker/settings.py
+++ hasker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from hasker.settings.include import secret
+from settings.include import secret
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
