7f6ed31b25057e848e4551686701843b59c21baa
aleksey <a.egorov@daiteco.ru>
2018-10-03 11:28:40 +0300

    update

--- build.sh
+++ build.sh
@@ -126,7 +126,7 @@ DB_PASSWORD = '${DB_PASSWORD}'
 EOF
 
 cd /home/work/hasker
-python3 manage.py makemigrations
+python3 manage.py makemigrations admin, auth, contenttypes, sessions, user, question
 python3 manage.py migrate
 
 
